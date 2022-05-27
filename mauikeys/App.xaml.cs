9033e36be01fc67a41cbb26a9ccee398edfc7bdc
Math <leroy.mathilde.marie@gmail.com>
2022-05-27 11:01:09 +0200

    init maui JM course

---
+++ mauikeys/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace mauikeys;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ mauikeys/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace mauikeys;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
